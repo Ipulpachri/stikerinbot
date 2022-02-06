fd66c54230bb77dea57904e88d3bc4113440c85d
SF-BOTZ <94295494+Ipulpachri@users.noreply.github.com>
2022-02-06 19:07:22 +0700

    menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -189,7 +189,7 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
       return conn.relayWAMessage(conn.prepareMessageFromContent(m.chat, {
         "listMessage": {
           "title": `${ucapan()}, ${name}`.trim(),
-          "description": "© stikerin",
+          "description": "© Fachri",
           "buttonText": "Klik Disini",
           "listType": "SINGLE_SELECT",
           "sections": [
@@ -413,16 +413,16 @@ function ucapan() {
   const time = moment.tz('Asia/Jakarta').format('HH')
   res = "Selamat dinihari"
   if (time >= 4) {
-    res = "Selamat pagi"
+    res = "Selamat pagi🌄"
   }
   if (time > 10) {
-    res = "Selamat siang"
+    res = "Selamat siang🌄"
   }
   if (time >= 15) {
-    res = "Selamat sore"
+    res = "Selamat sore🌆"
   }
   if (time >= 18) {
-    res = "Selamat malam"
+    res = "Selamat malam🌃"
   }
   return res
 }

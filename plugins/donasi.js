f7a8b33098c714ce34bc8e900ae86e5e01d9527a
SF-BOTZ <94295494+Ipulpachri@users.noreply.github.com>
2022-02-07 06:23:38 +0700

    donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, fla + 'don
 ├ Gopay [0895-2551-8325]
 ├ Dana [0812-8586-6502]
 └────
-`.trim(), '© Fachri', 'Donasi', '.donasi', m)
+`.trim(), '© Fachri', 'Owner', '.owner', m)
 handler.help = ['donasi']
 handler.tags = ['info']
 handler.command = /^dona(te|si)$/i
